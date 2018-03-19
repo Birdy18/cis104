404edc9a816228348a46177cd6c5c55d0c37813a
Trevor Birdsall <trevorbirdsall16@gmail.com>
2018-03-19 19:35:39 -0400

    Added more to music.js

--- music.js
+++ music.js
@@ -7,17 +7,27 @@
 "use strict"
 const PROMPT = require('readline-sync');
 
-/*HappyTunes is a progressive web application for downloading music files. Each time a file is purchased, a transaction record is created that includes the music genre and price paid. The available genres are Classical, Easy Listening, Jazz, Pop, Rock, and Other. Develop an application that accepts input data for each transaction and displays a report that lists each of the music genres, along with a count of the number of downloads in each of the following price categories:
-(Classical, Easy Listening, Jazz, Pop, Rock, and Other)
-    Over $10.00
-$6.00 through $9.99
-$3.00 through $5.99
-Under $3.00
-It should have the ability to sort by most downloaded genre.
-*/
+/**HappyTunes is a progressive web application for downloading music files. Each time a file is purchased, a transaction record is created that includes the music genre and price paid. The available genres are Classical, Easy Listening, Jazz, Pop, Rock, and Other. Develop an application that accepts input data for each transaction and displays a report that lists each of the music genres, along with a count of the number of downloads in each of the following price categories:
+ (Classical, Easy Listening, Jazz, Pop, Rock, and Other)
+ Over $10.00
+ $6.00 through $9.99
+ $3.00 through $5.99
+ Under $3.00
+ It should have the ability to sort by most downloaded genre.
+ */
 
 let runProgram = 1;
 let musicName, musicPrice;
+let errorCount = 0;
+let errorMax = 3;
+let login, account;
+let accounts = new Map();
+const accountPopulate = [
+    [1,"Trevor", 1998],
+    [2,"Rocky", 1158],
+    [3,"Jiminy",2014],
+    [4,"Howard",4120]
+];
 
 const MUSIC_LIST = new Map()
     .set('A',["Bronze-Rated (< $3.00)", chooseBronze])
@@ -37,7 +47,8 @@ class Music {
 }
 
 class Account {
-    constructor(name, PIN) {
+    constructor(id, name, PIN) {
+        this.id = id;
         this.name = name;
         this.pin = PIN;
     }
@@ -61,12 +72,77 @@ class Account {
             return false;
         }
     }
+
+    PINCheck(numPIN) {
+        if (this.pin === numPIN) {
+            return true;
+        }
+        return false;
+    }
 }
 class Transaction {
     constructor(account, reciept, total, date) {
         transaction.account = account.name;
         transaction.reciept
     }
+}
+
+
+function main() {
+    loadApp();
+    while(runProgram) {
+        activateApp();
+    }
+    console.log("Please come back, and rock on!! ")
+    return;
+}
+
+main();
+
+function loadApp() {
+    for(let account of accountPopulate) {
+        accounts.set(account[0], new Account(account[0],account[1],account[2],account[3]))
+    }
+}
+function runLogin() {
+    if (errorCount >= maxError) {
+        console.log("TOO MANY ERRORS.");
+        runProgram = 0;
+        return;
+    }
+
+    let id = Number(PROMPT.question('\nPlease type in your ID: '));
+    let name = PROMPT.question('Please enter in your name: ');
+    let numPIN = Number(PROMPT.question('\nPlease enter your PIN number: '));
+
+    if (!accounts.has(id)) {
+        console.log('ERROR INVALID CREDENTIALS!');
+        errorCount++;
+        return;
+    }
+
+    let foundAccount = accounts.get(id);
+
+    if (!foundAccount.name === name) {
+        console.log('ERROR INVALID CREDENTIALS!');
+        errorCount++;
+        return;
+    }
+    if (!foundAccount.PINCheck(numPIN)) {
+        console.log('ERROR INVALID CREDENTIALS!');
+        errorCount++;
+        return;
+    }
+    login = foundAccount;
+
+}
+
+function activateApp() {
+    if(!login) {
+        runLogin();
+        return;
+    }
+    manageMusicList();
 }
 
 function displayMusicList() {
@@ -85,6 +161,8 @@ function manageMusicList() {
     }
     MUSIC_LIST.get(option)[1]();
 }
+
+function
 
 function displayTransaction() {
 
